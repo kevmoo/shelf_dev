936ede391f5a1e4a1a9f895ac2b66b107df1abf8
Kevin Moore <kevmoo@google.com>
2021-07-19 12:51:44 -0700

    Prepare for v2

--- shelf_dev/CHANGELOG.md
+++ shelf_dev/CHANGELOG.md
@@ -1,3 +1,8 @@
+## 2.0.0
+
+- The server app is no longer "rooted" at the `source` configuration path. This
+  maps to be behavior of mapping Firebase hosting to a Cloud Run instance.
+
 ## 1.0.0
 
 - First release!
--- shelf_dev/lib/src/cli_options.g.dart
+++ shelf_dev/lib/src/cli_options.g.dart
@@ -1,7 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: deprecated_member_use_from_same_package, lines_longer_than_80_chars, prefer_expression_function_bodies
-
 part of 'cli_options.dart';
 
 // **************************************************************************
